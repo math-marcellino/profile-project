c241db55246080e173d1e9d5bcee3ededa74418f
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-06-18 13:47:35 +0700

    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,16 @@ import Head from 'next/head';
 import ProfileHero from '../components/ProfileHero';
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-          <title>Matthew Marcellino's Portofolio</title>
-          <meta name="description" content="Matthew Marcellino's Portofolio" />
-          <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ProfileHero/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Head>
+                <title>Matthew Marcellino's Portofolio</title>
+                <meta name="description" content="Matthew Marcellino's Portofolio" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <ProfileHero />
+        </div>
+    );
+};
 
-export default Home
+export default Home;
